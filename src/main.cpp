dd758bddea5522d72a077b7894d1ab1ea9cf7fc6
Paul Kendall <pkendall64@gmail.com>
2024-01-21 11:33:49 +1300

    A little bit of cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,19 @@ IRAM_ATTR void irq_handler(void) {
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(460800);
     delay(3000);
     Wire.begin(2, 4); // set master mode
+
+    // Compass init first
+    compass.init();
+	compass.setMode(0x01,0x08,0x10,0X00); // continuous, 100Hz, 8G, 512 over sample
+
+    Serial.println("CALIBRATING. Keep moving your sensor...");
+    compass.calibrate();
+    mRes = 0.73;
+
+    Serial.println("DONE.");
 
     // Initializing the ICM42607C
     int ret = IMU.begin();
@@ -46,60 +56,57 @@ void setup()
     IMU.startGyro(100, 2000);
     gRes = 2000.0/32768;
 
-    compass.init();
-    Serial.println("CALIBRATING. Keep moving your sensor...");
-    compass.calibrate();
-    mRes = 0.73;
-
-    Serial.println("DONE.");
 }
 
 void loop()
 {
+    static int counter = 0;
     if(irq_received) {
         irq_received = 0;
-        inv_imu_sensor_event_t imu_event;
 
-        // Get last event
+        inv_imu_sensor_event_t imu_event;
         IMU.getDataFromRegisters(&imu_event);
 
-        Serial.printf("ax: %7d ", imu_event.accel[0]);
-        Serial.printf("ay: %7d ", imu_event.accel[1]);
-        Serial.printf("az: %7d ", imu_event.accel[2]);
-
-        Serial.printf("gx: %7d ", imu_event.gyro[0]);
-        Serial.printf("gy: %7d ", imu_event.gyro[1]);
-        Serial.printf("gz: %7d ", imu_event.gyro[2]);
-
-        Serial.printf("T: %2.2fC ", (float)imu_event.temperature / 128 + 25);
-
-        int x, y, z, a;
-
-        // Read compass values
         compass.read();
 
-        // Return XYZ readings
+        int x, y, z;
         x = compass.getX();
         y = compass.getY();
         z = compass.getZ();
-
-        Serial.print("X: ");
-        Serial.print(x);
-        Serial.print(" Y: ");
-        Serial.print(y);
-        Serial.print(" Z: ");
-        Serial.print(z);
-
-        a = compass.getAzimuth();
-        Serial.print(" A: ");
-        Serial.print(a);
 
         MadgwickAHRSupdate(
             imu_event.gyro[0] * gRes * DEG_TO_RAD, imu_event.gyro[1] * gRes * DEG_TO_RAD, imu_event.gyro[2] * gRes * DEG_TO_RAD,
             imu_event.accel[0] * aRes, imu_event.accel[1] * aRes, imu_event.accel[2] * aRes,
             x * mRes, y * mRes, z * mRes
         );
-        Serial.printf(" R: %8.2f P: %8.2f Y: %8.2f ", getRoll(), getPitch(), getYaw());
-        Serial.println();
+
+        if (counter++ == 10)
+        {
+            counter = 0;
+#ifdef DEBUG_LOG
+            Serial.printf("ax: %7d ", imu_event.accel[0]);
+            Serial.printf("ay: %7d ", imu_event.accel[1]);
+            Serial.printf("az: %7d ", imu_event.accel[2]);
+
+            Serial.printf("gx: %7d ", imu_event.gyro[0]);
+            Serial.printf("gy: %7d ", imu_event.gyro[1]);
+            Serial.printf("gz: %7d ", imu_event.gyro[2]);
+
+            Serial.printf("T: %2.2fC ", (float)imu_event.temperature / 128 + 25);
+
+            Serial.print("X: ");
+            Serial.print(x);
+            Serial.print(" Y: ");
+            Serial.print(y);
+            Serial.print(" Z: ");
+            Serial.print(z);
+
+            int a = compass.getAzimuth();
+            Serial.print(" A: ");
+            Serial.print(a);
+#endif
+            Serial.printf(" R: %8.2f P: %8.2f Y: %8.2f", getRoll(), getPitch(), getYaw());
+            Serial.println();
+        }
     }
 }
